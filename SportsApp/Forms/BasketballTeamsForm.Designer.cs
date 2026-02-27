50a2497377b6fa077403575bdeeba274bc350dcd
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-27 00:49:07 -0500

    Updated Team Forms

--- SportsApp/Forms/BasketballTeamsForm.Designer.cs
+++ SportsApp/Forms/BasketballTeamsForm.Designer.cs
@@ -28,29 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.searchTextbox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sortByDropdown = new System.Windows.Forms.ComboBox();
+            this.sportInfoDataSet = new sportsApp.SportInfoDataSet();
+            this.nBATeamsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nBATeamsTableAdapter = new sportsApp.SportInfoDataSetTableAdapters.NBATeamsTableAdapter();
+            this.tableAdapterManager = new sportsApp.SportInfoDataSetTableAdapters.TableAdapterManager();
+            this.nBATeamsDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sportInfoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nBATeamsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nBATeamsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(254, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.Size = new System.Drawing.Size(161, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Basketball Teams";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.searchTextbox);
+            this.groupBox2.Location = new System.Drawing.Point(392, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 58);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // searchTextbox
+            // 
+            this.searchTextbox.Location = new System.Drawing.Point(45, 24);
+            this.searchTextbox.Name = "searchTextbox";
+            this.searchTextbox.Size = new System.Drawing.Size(100, 23);
+            this.searchTextbox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sortByDropdown);
+            this.groupBox1.Location = new System.Drawing.Point(12, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 58);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sort By";
+            // 
+            // sortByDropdown
+            // 
+            this.sortByDropdown.FormattingEnabled = true;
+            this.sortByDropdown.Items.AddRange(new object[] {
+            "Team Name",
+            "Conference",
+            "Wins",
+            "Losses",
+            "Ties",
+            "Stadium"});
+            this.sortByDropdown.Location = new System.Drawing.Point(40, 22);
+            this.sortByDropdown.Name = "sortByDropdown";
+            this.sortByDropdown.Size = new System.Drawing.Size(121, 23);
+            this.sortByDropdown.TabIndex = 3;
+            // 
+            // sportInfoDataSet
+            // 
+            this.sportInfoDataSet.DataSetName = "SportInfoDataSet";
+            this.sportInfoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // nBATeamsBindingSource
+            // 
+            this.nBATeamsBindingSource.DataMember = "NBATeams";
+            this.nBATeamsBindingSource.DataSource = this.sportInfoDataSet;
+            // 
+            // nBATeamsTableAdapter
+            // 
+            this.nBATeamsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.NBATeamsTableAdapter = this.nBATeamsTableAdapter;
+            this.tableAdapterManager.NFLTeamsTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = sportsApp.SportInfoDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.UsersTableAdapter = null;
+            // 
+            // nBATeamsDataGridView
+            // 
+            this.nBATeamsDataGridView.AutoGenerateColumns = false;
+            this.nBATeamsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.nBATeamsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn5});
+            this.nBATeamsDataGridView.DataSource = this.nBATeamsBindingSource;
+            this.nBATeamsDataGridView.Location = new System.Drawing.Point(0, 169);
+            this.nBATeamsDataGridView.MultiSelect = false;
+            this.nBATeamsDataGridView.Name = "nBATeamsDataGridView";
+            this.nBATeamsDataGridView.RowHeadersVisible = false;
+            this.nBATeamsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.nBATeamsDataGridView.Size = new System.Drawing.Size(604, 320);
+            this.nBATeamsDataGridView.TabIndex = 8;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "TeamName";
+            this.dataGridViewTextBoxColumn2.HeaderText = "TeamName";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Abbreviation";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Abbreviation";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Conference";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Conference";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Wins";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Wins";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "Losses";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Losses";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Arena";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Arena";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
             // 
             // BasketballTeamsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 491);
+            this.Controls.Add(this.nBATeamsDataGridView);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "BasketballTeamsForm";
             this.Text = "basketballTeamsForm";
+            this.Load += new System.EventHandler(this.BasketballTeamsForm_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sportInfoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nBATeamsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nBATeamsDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +211,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox searchTextbox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox sortByDropdown;
+        private SportInfoDataSet sportInfoDataSet;
+        private System.Windows.Forms.BindingSource nBATeamsBindingSource;
+        private SportInfoDataSetTableAdapters.NBATeamsTableAdapter nBATeamsTableAdapter;
+        private SportInfoDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView nBATeamsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
     }
 }
--- SportsApp/Forms/FootballTeamsForm.cs
+++ SportsApp/Forms/FootballTeamsForm.cs
@@ -16,5 +16,79 @@ namespace sportsApp.Forms
         {
             InitializeComponent();
         }
+
+        private void nFLTeamsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.nFLTeamsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.sportInfoDataSet);
+
+        }
+
+        private void FootballTeamsForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sportInfoDataSet.NFLTeams' table. You can move, or remove it, as needed.
+            this.nFLTeamsTableAdapter.Fill(this.sportInfoDataSet.NFLTeams);
+            sortByDropdown.SelectedIndex = 0;
+        }
+
+        // sort by drop down logic
+        private void sortByDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sortByDropdown.SelectedItem == null)
+            {
+                return;
+            }
+            else if ( sortByDropdown.SelectedItem == "Team Name")
+            {
+                nFLTeamsBindingSource.Sort = "TeamName";
+            }
+            else if ( sortByDropdown.SelectedItem == "Conference")
+            {
+                nFLTeamsBindingSource.Sort = "Conference";
+            }
+            else if ( sortByDropdown.SelectedItem == "Wins")
+            {
+                nFLTeamsBindingSource.Sort = "Wins DESC";
+            }
+            else if (sortByDropdown.SelectedItem == "Losses")
+            {
+                nFLTeamsBindingSource.Sort = "Losses DESC";
+            }
+            else if (sortByDropdown.SelectedItem == "Stadium")
+            {
+                nFLTeamsBindingSource.Sort = "Stadium";
+            }
+        }
+        private void searchTextbox_TextChanged(object sender, EventArgs e)
+        {
+            ApplySearchFilter();
+        }
+
+
+        private void ApplySearchFilter()
+        {
+            string searchText = searchTextbox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                nFLTeamsBindingSource.RemoveFilter();
+                return;
+            }
+
+            // Escape single quotes for RowFilter syntax
+            searchText = searchText.Replace("'", "''");
+
+            string filter =
+                $"TeamName LIKE '%{searchText}%' OR " +
+                $"Conference LIKE '%{searchText}%' OR " +
+                $"Stadium LIKE '%{searchText}%' OR " +
+                $"Convert(Wins, 'System.String') LIKE '%{searchText}%' OR " +
+                $"Convert(Losses, 'System.String') LIKE '%{searchText}%' OR " +
+                $"Convert(Ties, 'System.String') LIKE '%{searchText}%'";
+
+            nFLTeamsBindingSource.Filter = filter;
+        }
+
     }
 }
